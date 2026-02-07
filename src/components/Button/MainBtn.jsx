0d5fcc78549664a7f6b4a60a7f86ff8098076711
Dawood Butt <buttdaud94@gmail.com>
2026-02-07 20:24:08 +0500

    btn

--- src/components/Button/MainBtn.jsx
+++ src/components/Button/MainBtn.jsx
@@ -10,9 +10,8 @@ const MainBtn = ({ children, text, onClick, className = "" }) => {
   md:px-4 px-2 py-2.5 font-semibold shadow-md border-2 border-transparent 
   transition-all duration-300 cursor-pointer
   bg-black text-white
-  hover:bg-yellow-500 hover:text-black hover:border-yellow-500 hover:scale-105
-  active:bg-yellow-500 active:text-black active:border-yellow-500
-  focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2
+  hover:bg-yellow-500 hover:text-black hover:scale-105
+  active:bg-yellow-500 active:text-black active:scale-100
   ${className}
 `}
     >
