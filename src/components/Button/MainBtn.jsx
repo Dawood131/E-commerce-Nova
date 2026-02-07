cce7f3304f3f15444f7ee808a9e4e38eb53761da
Dawood Butt <buttdaud94@gmail.com>
2026-02-07 20:43:10 +0500

    btn color

--- src/components/Button/MainBtn.jsx
+++ src/components/Button/MainBtn.jsx
@@ -7,11 +7,7 @@ const MainBtn = ({ children, text, onClick, className = "" }) => {
       onClick={onClick}
       aria-label={text || "Button"}
       className={`
-  md:px-4 px-2 py-2.5 font-semibold shadow-md border-2 border-transparent 
-  transition-all duration-300 cursor-pointer
-  bg-black text-white
-  hover:bg-yellow-500 hover:text-black hover:scale-105
-  active:bg-yellow-500 active:text-black active:scale-100
+         md:px-4 px-2 py-2.5 font-semibold shadow-md border-2 border-transparent transition-all duration-300 cursor-pointer bg-black text-white hover:bg-yellow-500 hover:text-black hover:border-yellow-500 hover:scale-105 active:bg-yellow-500 active:text-black active:border-yellow-500
   ${className}
 `}
     >
